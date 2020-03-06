25173f3ce7312a140056a3d56fddd564db3ae77e
Dalton Nickerson <dalton.nickerson@gmail.com>
2020-03-06 00:43:15 -0500

    Marker clustering.

--- get_voting_locations.js
+++ get_voting_locations.js
@@ -14,28 +14,35 @@ function json(response) {
 
 
 async function get_voting_locations() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    let map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 33.247875, lng: -83.441162},
-        zoom: 8
+        zoom: 8,
+        minZoom: 7,
+        maxZoom: 18
     });
-    markers = []
-    let result = fetch('./geocode_place_id_cache.json')
+    let markers = []
+    fetch('./geocode_place_id_cache.json')
         .then(status)
         .then(json)
         .then((data) => {
-            console.log("Loaded temporary cache of "+data.length+" polling locations.");
+            console.log("Loaded temporary cache of " + data.length + " polling locations.");
             return data;
         })
         .then((data) => {
-           for (let i = 0; i < data.length; i++){
-               let result = data[i];
-               let latLng = new google.maps.LatLng(result['lat'], result['lng'])
-               let marker = new google.maps.Marker({
-                   position: latLng,
-                   map: map,
-                   name: result['pollPlaceName']
-               });
-           }
+            for (let i = 0; i < data.length; i++) {
+                let result = data[i];
+                let latLng = new google.maps.LatLng(result['lat'], result['lng'])
+                let marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map,
+                    label: result['pollPlaceName']
+                });
+                markers.push(marker);
+            }
+            let markerCluster = new MarkerClusterer(map, markers,
+                {imagePath: './markerclustererplus/images/m',
+                 maxZoom: map.maxZoom - 1});
+            return markerCluster;
         });
 }
 
