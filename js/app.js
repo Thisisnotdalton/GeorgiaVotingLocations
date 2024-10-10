64002df39979ee094be96e736401384454242ed4
Dalton Nickerson <dalton.nickerson@gmail.com>
2024-10-10 15:29:37 -0400

    Dataset handler for county data and scenarios.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,32 @@
 import Map from "./map_utils.js";
 import DataSet from "./dataset.js";
 
-export async function Start(){
+export async function Start() {
     let map = new Map(
-        "map", 
-        'https://demotiles.maplibre.org/style.json', 
+        "map",
+        'https://demotiles.maplibre.org/style.json',
         [0, 0],
         1);
     let data = new DataSet();
-    console.log(await data.getCounties());
+    let counties = await data.getCounties();
+    let scenarioNames = await data.getScenarioNames();
+    console.log(scenarioNames);
+    for (const countyName of counties) {
+        for (let scenarioName of scenarioNames) {
+            let availableDates = await data.getScenarioDates(scenarioName);
+            for (let scenarioDate of availableDates) {
+                let availablePollingPlaces = await data.getPollingPlaces(scenarioName, scenarioDate, countyName);
+                if (availablePollingPlaces.length == 0) {
+                    availablePollingPlaces = 'None.'
+                }else{
+                    availablePollingPlaces = availablePollingPlaces.map((x)=>`"${x['name']}"`);
+                }
+                console.log(`Scenario ${scenarioName}, ${scenarioDate}, ${countyName}: ${availablePollingPlaces}`);
+            }
+        }
+    }
 }
 
-window.onload = async function() {
+window.onload = async function () {
     await Start();
 }
--- js/dataset.js
+++ js/dataset.js
@@ -30,15 +30,59 @@ class JSONCache {
 
 class DataSet {
     #jsonCache;
+    #electionID = 'a0p3d00000LWdF5AAL';
+    #dataPath;
 
     constructor() {
         this.#jsonCache = new JSONCache();
+        this.#dataPath = `./data/${this.#electionID}/`
     }
 
     async getCounties() {
-        return this.#jsonCache.getJSON('./counties.json');
+        return this.#jsonCache.getJSON(`${this.#dataPath}/counties.json`);
     }
 
+    async #getGeoJSON(countyName) {
+        return this.#jsonCache.getJSON(`${this.#dataPath}/geojson/${countyName}.geojson`);
+    }
+
+    async #getJSON(countyName) {
+        return this.#jsonCache.getJSON(`${this.#dataPath}/json/${countyName}.json`)
+    }
+
+    async #getScenariosJSON() {
+        return this.#jsonCache.getJSON(`${this.#dataPath}/scenarios/scenarios.json`);
+    }
+
+    async getScenarioNames() {
+        return Object.keys(await this.#getScenariosJSON());
+    }
+
+    async #getAllScenariosData(scenarioName) {
+        return (await this.#getScenariosJSON())[scenarioName];
+    }
+
+    async getScenarioDates(scenarioName) {
+        return Object.keys((await this.#getAllScenariosData(scenarioName)));
+    }
+
+    async #getScenarioData(scenarioName, scenarioDate, countyName = '') {
+        let scenarioData = (await this.#getAllScenariosData(scenarioName))[scenarioDate];
+        if (countyName.length > 0) {
+            scenarioData = scenarioData[countyName];
+        }
+        return scenarioData;
+    }
+
+    async getPollingPlaces(scenarioName, scenarioDate, countyName, geojson = false) {
+        let pollingPlaces = [];
+        const countyJSON = await (geojson ? this.#getGeoJSON(countyName) : this.#getJSON(countyName));
+        const availablePollingPlaces = await this.#getScenarioData(scenarioName, scenarioDate, countyName);
+        for (let pollingPlaceIndex of availablePollingPlaces) {
+            pollingPlaces.push(countyJSON[pollingPlaceIndex]);
+        }
+        return Promise.resolve(pollingPlaces);
+    }
 }
 
 export default DataSet;
