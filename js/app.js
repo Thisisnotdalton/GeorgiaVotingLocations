9829a43058291009c1c22eb5f7a9c192a3ac4ffc
Dalton Nickerson <dalton.nickerson@gmail.com>
2024-10-10 18:42:52 -0400

    WIP for validating scenario parameters.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 import Map from "./map_utils.js";
 import DataSet from "./dataset.js";
 
+class ScenarioSelector {
+    #data;
+    #counties;
+    #scenarioNames;
+    
+    constructor() {
+        this.#data = new DataSet();
+    }
+
+    async initialize() {
+        this.#counties = await this.#data.getCounties();
+        this.#scenarioNames = await this.#data.getScenarioNames();
+    }
+}
+
 export async function Start() {
     let map = new Map(
         "map",
         'https://demotiles.maplibre.org/style.json',
         [0, 0],
         1);
-    let data = new DataSet();
+    let data = new DataSet(); //new ScenarioSelector();
     let counties = await data.getCounties();
     let scenarioNames = await data.getScenarioNames();
     console.log(scenarioNames);
@@ -18,8 +33,8 @@ export async function Start() {
                 let availablePollingPlaces = await data.getPollingPlaces(scenarioName, scenarioDate, countyName);
                 if (availablePollingPlaces.length == 0) {
                     availablePollingPlaces = 'None.'
-                }else{
-                    availablePollingPlaces = availablePollingPlaces.map((x)=>`"${x['name']}"`);
+                } else {
+                    availablePollingPlaces = availablePollingPlaces.map((x) => `"${x['name']}"`);
                 }
                 console.log(`Scenario ${scenarioName}, ${scenarioDate}, ${countyName}: ${availablePollingPlaces}`);
             }
--- js/dataset.js
+++ js/dataset.js
@@ -27,11 +27,57 @@ class JSONCache {
     }
 }
 
+class StringValueSet {
+    #validValues = null;
+    #valueIndices = null;
+    #defaultValue = null;
+    #setDescription = null;
+    #setCase = null;
+
+    constructor(values, setDescription, defaultValue = null, setCase = undefined) {
+        this.#setDescription = setDescription;
+        this.#setCase = StringValueSet.#applyCase(setCase);
+        this.#validValues = values.map(this.#setCase);
+        this.#defaultValue = defaultValue == null ? this.#validValues[0] : defaultValue;
+        this.#valueIndices = {};
+        for (const index in this.#validValues) {
+            this.#valueIndices[this.#validValues[index]] = index;
+        }
+    }
+
+    static #applyCase(setCase) {
+        if (setCase != undefined) {
+            if (setCase == 'upper') {
+                return (x) => x.toUpperCase();
+            } else if (setCase == 'lower') {
+                return (x) => x.toLowerCase();
+            }
+        }
+        return (x) => x;
+    }
+
+    normalize(value) {
+        value = this.#setCase(value);
+        if (value in this.#valueIndices) {
+            return value;
+        }
+        console.log(`Failed to validate ${this.#setDescription} "${value}". Defaulting to "${this.#defaultValue}".`);
+        return this.#defaultValue;
+    }
+
+    values() {
+        return Array.from(this.#validValues);
+    }
+}
+
 
 class DataSet {
     #jsonCache;
     #electionID = 'a0p3d00000LWdF5AAL';
     #dataPath;
+    #counties = null;
+    #scenarioNames = null;
+    #scenarioDates = null;
 
     constructor() {
         this.#jsonCache = new JSONCache();
@@ -39,7 +85,11 @@ class DataSet {
     }
 
     async getCounties() {
-        return this.#jsonCache.getJSON(`${this.#dataPath}/counties.json`);
+        if (this.#counties == null) {
+            let values = await this.#jsonCache.getJSON(`${this.#dataPath}/counties.json`);
+            this.#counties = new StringValueSet(values, 'County', 'all_voting_locations', 'upper')
+        }
+        return this.#counties.values();
     }
 
     async #getGeoJSON(countyName) {
@@ -55,23 +105,36 @@ class DataSet {
     }
 
     async getScenarioNames() {
-        return Object.keys(await this.#getScenariosJSON());
+        if (this.#scenarioNames == null) {
+            let values = Object.keys(await this.#getScenariosJSON());
+            this.#scenarioNames = new StringValueSet(values, 'Scenario Name');
+        }
+        return this.#scenarioNames.values();
     }
 
     async #getAllScenariosData(scenarioName) {
-        return (await this.#getScenariosJSON())[scenarioName];
+        await this.getCounties();
+        await this.getScenarioNames();
+        return (await this.#getScenariosJSON())[this.#scenarioNames.normalize(scenarioName)];
     }
 
     async getScenarioDates(scenarioName) {
-        return Object.keys((await this.#getAllScenariosData(scenarioName)));
+        if (this.#scenarioDates == null) {
+            this.#scenarioDates = {};
+        }
+        if (!(scenarioName in this.#scenarioDates)) {
+            let values = Object.keys(await this.#getAllScenariosData(scenarioName));
+            this.#scenarioDates[scenarioName] = new StringValueSet(values, 'Scenario Date');
+        }
+        return this.#scenarioDates[scenarioName].values();
     }
 
     async #getScenarioData(scenarioName, scenarioDate, countyName = '') {
-        let scenarioData = (await this.#getAllScenariosData(scenarioName))[scenarioDate];
-        if (countyName.length > 0) {
-            scenarioData = scenarioData[countyName];
-        }
-        return scenarioData;
+        await this.getScenarioDates(scenarioName);
+        scenarioName = this.#scenarioNames.normalize(scenarioName);
+        scenarioDate = this.#scenarioDates[scenarioName].normalize(scenarioDate);
+        countyName = this.#counties.normalize(countyName);
+        return Promise.resolve((await this.#getAllScenariosData(scenarioName))[scenarioDate][countyName]);
     }
 
     async getPollingPlaces(scenarioName, scenarioDate, countyName, geojson = false) {
