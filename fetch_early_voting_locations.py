6b9d690cf25b2a09eb3ffe764368a5199ab03826
Dalton Nickerson <dalton.nickerson@gmail.com>
2024-10-12 17:53:16 -0400

    Allow letters in address_number, spaces in place/town names.

--- fetch_early_voting_locations.py
+++ fetch_early_voting_locations.py
@@ -209,7 +209,7 @@ def fetch_early_voting_locations(
     return locations
 
 
-address_re = re.compile(r'(?P<address_number>\d*)\s+(?P<street>[^,]+),\s*(?P<place>[A-Za-z]+)')
+address_re = re.compile(r'(?P<address_number>[\da-zA-Z]*)\s+(?P<street>[^,]+),\s*(?P<place>[A-Za-z\s]+)')
 postcode_re = re.compile(r'\s+(\d+[- ]?\d*)$')
 
 
